4a77dfab82513eb086398f5086cb02015c595cf4
naotakke <kyogoku@guildworks.jp>
2023-11-25 15:23:08 +0900

    day5 learning

---
+++ src/app/api/images/route.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+
+import { supabase } from '../../../../lib/supabaseClient'
+
+export async function POST(request: Request) {
+  const formData = await request.formData()
+  const file = formData.get('file') as File
+
+  const response = await supabase.storage
+    .from('learning-phase') // target bucket name
+    .upload(file.name, file)
+
+  return NextResponse.json(response)
+}
+
+export async function GET() {
+  const response = await supabase.storage
+    .from('learning-phase') // target bucket name
+    .list('', {
+      limit: 100,
+      offset: 0,
+      sortBy: { column: 'name', order: 'asc' },
+    })
+
+  return NextResponse.json(response)
+}
